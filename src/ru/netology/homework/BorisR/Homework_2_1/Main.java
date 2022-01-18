2f28ac11031dec506ee7409c7e5d1fc3419418ef
BorisR69 <jhbcH@21>
2022-01-18 21:17:25 +0300

    Версия 2

--- src/ru/netology/homework/BorisR/Homework_2_1/Main.java
+++ src/ru/netology/homework/BorisR/Homework_2_1/Main.java
@@ -11,26 +11,20 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner choice = new Scanner(System.in);
-//        int productNumber;
-//        int productCount;
-        boolean buyOn = true;
-
         System.out.println("Список возможных товаров для покупки:"); // Вывод списка товаров
         for (int i = 0; i < prices.length; i++) {
             System.out.println(i + 1 + ". " + products[i] + " - " + prices[i] + " руб.");
         }
-        while (buyOn) {
+        while (true) {
             System.out.println("Выберите товар и количество или введите `end`:");
             System.out.println("В вашей корзине покупок на сумму " + sumProducts + " руб.");
             String input = choice.next();
             if ("end".equals(input)) {
-                buyOn = false;
+            //    buyOn = false;
                 busketFinal(); // Вывод финальной корзины
-                System.out.println("Удачных покупок в будущем. До свидания.");
+                System.out.println("Удачных покупок в будущем. \nДо свидания.");
                 break; // Выход из программы
             }
-//            productNumber = Integer.parseInt(input);
-//            productCount = Integer.parseInt(choice.next());
             busketIn (Integer.parseInt(input), Integer.parseInt(choice.next())); // Наполнение корзины
         }
     }
@@ -44,8 +38,7 @@ public class Main {
         System.out.println("Ваша корзина:\n" + "Наименование товара   Количество  Цена/за.ед  Общая стоимость");
         for (int j = 0; j < products.length; j++){
             if (busket[j] != 0) {
-                System.out.print(products[j] + "-" + busket[j] + "-" + prices[j] + "-" + prices[j]*busket[j] +
-                        "\n");
+                System.out.printf("%-21s %-11s %-11s %-15s \n", products[j], busket[j], prices[j], prices[j]*busket[j]);
             }
         }
         System.out.println("Всего покупок на сумму: " + sumProducts + " руб.");
